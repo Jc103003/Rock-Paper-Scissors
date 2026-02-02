80569c7127be90775111104d47c01ef535645725
James Caldwell <james.c103003@gmail.com>
2026-02-01 19:21:48 -0500

    created UI
    added DOM to update and create UI for RPS game

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,13 @@
 let humanScore = 0
 let computerScore = 0
 
+// event listerns
+let rockButton = document.getElementById("rockButton")
+let paperButton = document.getElementById("paperButton")
+let scissorsButton = document.getElementById("scissorsButton")
+let results = document.querySelector(".results")
+let scoreText = document.querySelector('.score')
+
 // Randomly return either rock, paper, or scissors
 function getComputerChoice() {
     const randomValue = Math.random()
@@ -18,11 +25,6 @@ function getComputerChoice() {
     }
 }
 
-// Get human decision
-function getHumanChoice(){
-    return prompt("What is your choice")
-}
-
 
 function playRound(humanChoice, computerChoice) {
     
@@ -33,29 +35,28 @@ function playRound(humanChoice, computerChoice) {
     };
 
     if (humanChoice.toLowerCase() == computerChoice) {
-        console.log("Tie!");
+        results.textContent = "Tie!"
     }
     else if (winsAgainst[humanChoice.toLowerCase()] == computerChoice ) {
-        console.log("You win! " + humanChoice + " beats " + computerChoice)
+        results.textContent = "You win! " + humanChoice + " beats " + computerChoice
         humanScore += 1
     }
     else {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice)
+        results.textContent = "You lose! " + computerChoice + " beats " + humanChoice
         computerScore += 1
     }
 }
 
-function playGame() {
-
-    for (i = 1; i <= 5; i++) {
-        let computerChoice = getComputerChoice()
-        let humanChoice = getHumanChoice()  
-        playRound(humanChoice, computerChoice)
-    }
-     console.log("Human = " + humanScore + " Robot = " + computerScore)
+function playGame(humanChoice) {
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+    scoreText.textContent = "Human = " + humanScore + " Robot = " + computerScore
+    
 }
 
-playGame()
+rockButton.addEventListener('click', () => playGame("rock"))
+paperButton.addEventListener('click', () => playGame("paper"))
+scissorsButton.addEventListener('click', () => playGame("scissors"))
 
 
 
